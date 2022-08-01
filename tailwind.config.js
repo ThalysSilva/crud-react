428afc4036c141dc12d33ef758ef85420704e2f8
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-01 03:02:27 -0300

    chore: adding tailwind in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
