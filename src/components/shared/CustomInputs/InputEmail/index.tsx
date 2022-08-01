145881ef89e7001ff7c33d7a03105ecfea4c9127
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-01 00:30:26 -0300

    feat: adding inputEmail

---
+++ src/components/shared/CustomInputs/InputEmail/index.tsx
@@ -0,0 +1,51 @@
+import React, { ChangeEvent, ReactElement } from "react";
+
+import useInputEmail from "./hook/useInputEmail";
+import { InputModern } from "../../InputModern";
+import { ValidatorFunction } from "../../../../validators/types";
+
+interface InputEmailProps {
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  validatorFunctions?: ValidatorFunction[],
+  hideValidationBar?: boolean;
+  onRightIcon?: ReactElement;
+  onLeftIcon?: ReactElement;
+  isRequired?: boolean;
+  placeholder?: string;
+  dataTestId?: string;
+  emailName?: string;
+  className?: string;
+  id?: string;
+}
+
+export function InputEmail({
+  placeholder = "E-mail",
+  emailName = "email",
+  validatorFunctions,
+  hideValidationBar,
+  onRightIcon,
+  isRequired,
+  onLeftIcon,
+  dataTestId,
+  className,
+  id,
+}: InputEmailProps) {
+  const { ValidatorFunctionArray } = useInputEmail(validatorFunctions);
+
+  return (
+    <InputModern
+      validateFunctions={ValidatorFunctionArray}
+      hideValidationBar={hideValidationBar}
+      placeholder={placeholder}
+      onRightIcon={onRightIcon}
+      dataTestId={dataTestId}
+      onLeftIcon={onLeftIcon}
+      required={isRequired}
+      className={className}
+      name={emailName}
+      maxLength={100}
+      type={"email"}
+      id={id}
+    />
+  );
+}
