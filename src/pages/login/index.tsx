6edf525f3d6496dfc4fd8cf799ce8d60669d8a0a
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-01 03:03:51 -0300

    feat: implements new stuffs to pageLogin

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
+import AvatarIcon from '../../assets/icon/Avatar';
+import LockIcon from '../../assets/icon/Lock';
 import { Button } from '../../components/shared/Button';
 import { Container } from '../../components/shared/Container';
 import { InputEmail } from '../../components/shared/CustomInputs/InputEmail';
 import { InputPassword } from '../../components/shared/CustomInputs/InputPassword';
-import { H1 } from '../../components/shared/Texts';
+import { H1, H3 } from '../../components/shared/Texts';
 
 export default function Login() {
   return (
-    <Container>
-      <div>
-        <H1>{'Login'}</H1>
-        <div>
-          <InputEmail />
-          <InputPassword />
+    <Container className={
+      'flex flex-col justify-center sm:justify-between relative w-full h-screen sm:h-fit sm:w-[450px] sm:rounded-2xl sm:shadow-2xl sm:border sm:border-white/50 min-w-[360px] bg-transparent p-4'
+    } >
+      <form className={' w-auto'} onSubmit={() => {}}>
+        <div className={'flex w-full justify-center items-center mb-6'}>
+          <H1>{'Login'}</H1>
         </div>
-        <Button>
-          <div>{'Entrar'}</div>
-        </Button>
-      </div>
+        <div className={'mb-6'}>
+          <InputEmail
+            dataTestId={'inputEmailLogin'}
+            onLeftIcon={<AvatarIcon />}
+            id={'email'}
+            isRequired
+          />
+        </div>
+        <div className={'mb-2'}>
+          <InputPassword
+            dataTestId={'inputPasswordLogin'}
+            onLeftIcon={<LockIcon />}
+            id={'password'}
+            isRequired
+            showButton
+          />
+        </div>
+        <div className={'mb-20'}/>
+        <div className={'flex flex-row justify-end items-end'}>
+          <Button dataTestId={'buttonLogin'} isLoading={false}>
+            <H3 className={'flex h-10 justify-center items-center'}>{`Entrar`}</H3>
+          </Button>
+        </div>
+      </form>
     </Container>
   );
 }
