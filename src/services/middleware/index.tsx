b4f44c7253ae1b6b9d226b42e5d896c38c14b2d7
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-01 13:19:23 -0300

    feat: refactor middleware to catch token

--- src/services/middleware/index.tsx
+++ src/services/middleware/index.tsx
@@ -1,11 +1,25 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import axios from 'axios';
+import useCookies from '../../hooks/useCookies';
 
 function middleware() {
-  function requestAxios() {
+  const { getCookie } = useCookies();
+  function requestAxios(withAuth: boolean | null = null) {
     const request = axios.create({
       baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-      headers: { Accept: 'application/json' }
+      headers: { Accept: 'application/json', 'Access-Control-Allow-Origin': '*' }
     });
+
+    if (withAuth) {
+      const token = getCookie('token');
+      if (token) {
+        request.interceptors.request.use((requestConfig) => {
+          requestConfig = { ...requestConfig };
+          requestConfig.headers = { ...requestConfig.headers, token: token };
+          return requestConfig;
+        });
+      }
+    }
 
     return request;
   }
