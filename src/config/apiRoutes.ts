4025715926f4c33310bbeed18bdef9a4755c3f2c
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-01 00:40:09 -0300

    feat: adding config structure and ApiRoutes object

---
+++ src/config/apiRoutes.ts
@@ -0,0 +1,12 @@
+const ApiRoutes = {
+  users: {
+    login: '/login',
+    loginAdm: '/login/adm'
+  },
+  events: {
+    dailyEvents: '/eventos_diarios',
+    dailyEventDetail: '/eventos_diarios/{id}'
+  }
+};
+
+export default ApiRoutes;
