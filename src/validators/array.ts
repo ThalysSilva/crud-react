ae3715fdc920188f618c194dd70ef1f4045e2b23
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-01 00:24:27 -0300

    chore: ident

--- src/validators/array.ts
+++ src/validators/array.ts
@@ -1,41 +1,53 @@
-import { validationResponse } from "./types";
+import { validationResponse } from './types';
 
 export function validateArrayLength(expectedLength: number, array: any[]): validationResponse {
   if (!!array && array.length && array.length === expectedLength) {
-    return {isValid: true}
+    return { isValid: true };
   }
 
-  return {isValid:false, reason: "O tamanho do campo é diferente do atual."};
-};
+  return { isValid: false, reason: 'O tamanho do campo é diferente do atual.' };
+}
 
-export function validateArrayMaxLength(expectedMaxLength: number, array: any[]): validationResponse {
+export function validateArrayMaxLength(
+  expectedMaxLength: number,
+  array: any[]
+): validationResponse {
   if (!!array && array.length && array.length <= expectedMaxLength) {
-    return {isValid: true}
+    return { isValid: true };
   }
 
-  return {isValid:false, reason: "O tamanho do campo é maior que o esperado."};
-};
+  return { isValid: false, reason: 'O tamanho do campo é maior que o esperado.' };
+}
 
-export function validateArrayMinLength(expectedMinLength: number, array: any[]): validationResponse {
+export function validateArrayMinLength(
+  expectedMinLength: number,
+  array: any[]
+): validationResponse {
   if (!!array && array.length && array.length >= expectedMinLength) {
-    return {isValid: true}
+    return { isValid: true };
   }
 
-  return {isValid:false, reason: "O tamanho do campo é menor que o esperado."};
-};
+  return { isValid: false, reason: 'O tamanho do campo é menor que o esperado.' };
+}
 
-export function validateArrayMaxOrEqualLength(expectedMaxOrEqualLength: number, array: any[]): validationResponse {
+export function validateArrayMaxOrEqualLength(
+  expectedMaxOrEqualLength: number,
+  array: unknown[]
+): validationResponse {
   if (!!array && array.length && array.length < expectedMaxOrEqualLength) {
-    return {isValid: true}
+    return { isValid: true };
   }
 
-  return {isValid:false, reason: "O tamanho do campo não é maior que o esperado."};
-};
+  return { isValid: false, reason: 'O tamanho do campo não é maior que o esperado.' };
+}
 
-export function validateArrayMinOrEqualLength(expectedMinOrEqualLength: number, array: any[]): validationResponse {
+export function validateArrayMinOrEqualLength(
+  expectedMinOrEqualLength: number,
+  array: any[]
+): validationResponse {
   if (!!array && array.length && array.length > expectedMinOrEqualLength) {
-    return {isValid: true}
+    return { isValid: true };
   }
 
-  return {isValid:false, reason: "O tamanho do campo é menor que o esperado."};
-};
+  return { isValid: false, reason: 'O tamanho do campo é menor que o esperado.' };
+}
