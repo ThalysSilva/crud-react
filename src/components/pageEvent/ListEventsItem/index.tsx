dc5e81339d8a7b23b13c6585a0970767da6fc121
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-02 20:29:11 -0300

    feat: add redirect to EventDetail page

--- src/components/pageEvent/ListEventsItem/index.tsx
+++ src/components/pageEvent/ListEventsItem/index.tsx
@@ -1,6 +1,8 @@
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import PenIcon from '../../../assets/icon/Pen';
 import TrashIcon from '../../../assets/icon/Trash';
+import config from '../../../config';
 import { H2 } from '../../shared/Texts';
 import { ListEventsItemContent } from '../ListIEventstemContent';
 import { DailyEvent } from './types';
@@ -11,6 +13,13 @@ type Props = {
 
 export function ListEventsItem({ evento }: Props) {
   const [color, setColor] = useState('#fff');
+  const { events } = config.webRoutes;
+
+  const router = useRouter();
+
+  function editItem(id: string) {
+    router.push(events.base + '/' + id);
+  }
   useEffect(() => {
     setColor(evento.cor);
   }, [evento.cor]);
@@ -21,7 +30,7 @@ export function ListEventsItem({ evento }: Props) {
         <div className={'flex flex-row justify-between items-center mb-4'}>
           <H2>{evento.titulo}</H2>
           <div className={'flex flex-row justify-center items-start gap-6'}>
-            <button>
+            <button onClick={() => editItem(evento.id.toString())}>
               <PenIcon />
             </button>
             <button>
