32c8b3d8e429010ed108558d0009d75794ac3a6e
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-02 20:25:25 -0300

    feat: adding animation to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        heightModalAnimation: {
+          "0%": {
+            maxHeight: "0px",
+
+            opacity: 0,
+          },
+
+          "100%": {
+            maxHeight: "476px",
+
+            opacity: 1,
+          },
+        },
+
+        fadeInAnimation: {
+          "0%": { opacity: 0 },
+
+          "100%": { opacity: 1 },
+        },
+      },
+
+      animation: {
+        heightModalAnimation: "heightModalAnimation 500ms ease-in-out",
+        fadeInAnimation:" fadeInAnimation 200ms ease-in-out"
+      },
+    },
   },
   plugins: [],
 }
