dcecd71564d67904e631cdef2735629156222e61
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-03 18:56:04 -0300

    feat: use hook useCreateEvent and change some stuffs unused

--- src/pages/events/create/index.tsx
+++ src/pages/events/create/index.tsx
@@ -3,45 +3,44 @@ import React from 'react';
 
 import WithAuthentication from '../../../components/shared/WithAuthentication';
 import { InputNumber } from '../../../components/shared/CustomInputs/InputNumber';
-import useEditEvent from '../../../hooks/pageEditEvent/useEditEvent';
-import LoadingSpinner from '../../../assets/icon/LoadingSpinner';
+import useCreateEvent from '../../../hooks/pageCreateEvent/useCreateEvent';
 import { TextArea } from '../../../components/shared/textArea';
+import { Button } from '../../../components/shared/Button';
 import { Input } from '../../../components/shared/Input';
-import { When } from '../../../components/shared/When';
+import { H4 } from '../../../components/shared/Texts';
 import { Modal } from '../../../components/Modal';
 import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
-import useCreateEvent from '../../../hooks/pageCreateEvent/useCreateEvent';
 
 function PageEditEvent() {
-  const router = useRouter();
-  const { eventId } = router.query;
   const {
     handleCloseCalendar,
     normalizedDateEvent,
     handleOpenCalendar,
     setDateEvent,
     showCalendar,
+    handleSubmit,
     isLoading,
     dateEvent,
-    evento
-  } = useCreateEvent(eventId as string);
+  } = useCreateEvent();
 
   return (
-    <div className={'flex flex-col p-4 w-full h-screen items-center justify-center bg-gray-900'}>
-      <When value={false}>
-        <LoadingSpinner />
-      </When>
-      <When value={true}>
+    <div className={'flex flex-col w-full h-screen sm:w-[470px]  items-center justify-center '}>
+      <form
+        className={
+          'flex flex-col items-center justify-center w-full h-screen sm:h-fit bg-gray-900 p-8 sm:rounded-xl'
+        }
+        onSubmit={handleSubmit}
+      >
         <div className={`relative grid grid-cols-4 h-fit w-full flex-row items-start gap-6 mb-6`}>
-          <div className={'flex flex-1 relative top-2 col-span-1 '}>{'Título:'}</div>
+          <H4 className={'flex flex-1 relative top-2 col-span-1 '}>{'Título:'}</H4>
           <div className={'col-span-3'}>
-            <Input required defaultValue={evento?.titulo} id={'eventTitle'} autoComplete={'off'} />
+            <Input required id={'eventTitle'} autoComplete={'off'} />
           </div>
         </div>
 
         <div className={'grid grid-cols-4 h-fit w-full flex-row items-center gap-6 mb-6'}>
-          <div className={'col-span-1'}>{'Data:'}</div>
+          <H4 className={'col-span-1'}>{'Data:'}</H4>
           <div className={'col-span-1'}>
             <button
               onClick={handleOpenCalendar}
@@ -53,31 +52,33 @@ function PageEditEvent() {
           </div>
         </div>
         <div className={'grid grid-cols-4 h-fit w-full flex-row items-center gap-6 mb-6'}>
-          <div className={'col-span-1'}>{'Hora:'}</div>
+          <H4 className={'col-span-1'}>{'Hora:'}</H4>
           <div className={'col-span-1'}>
-            <InputNumber min={0} max={24} maxLength={2} defaultValue={'00'} className={'w-[55px]'} id={'eventHour'} />
+            <InputNumber min={0} max={24} maxLength={2} className={'w-[55px]'} id={'eventHour'} />
           </div>
-          <div className={'col-span-1'}>{'Minuto:'}</div>
+          <H4 className={'col-span-1'}>{'Minuto:'}</H4>
           <div className={'col-span-1'}>
-            <InputNumber min={0} max={60} maxLength={2} defaultValue={'00'} className={'w-[55px]'} id={'eventMinute'} />
+            <InputNumber min={0} max={60} maxLength={2} className={'w-[55px]'} id={'eventMinute'} />
           </div>
         </div>
-        <div className={'flex h-fit w-full flex-col items-center gap-6 mb-6 outline-none'}>
-          <div>{'Descrição'}</div>
+        <div className={'flex h-fit w-full flex-col items-center gap-6 mb-20 outline-none'}>
+          <H4>{'Descrição'}</H4>
           <div className={'w-full '}>
             <TextArea
               className={' h-[80px] w-full'}
               id={'eventDescription'}
-              defaultValue={evento?.descricao}
             />
           </div>
         </div>
-        <Modal closeModal={handleCloseCalendar} show={showCalendar} closeOnBackDrop>
-          <div className="rounded-xl">
-            <Calendar value={dateEvent} onChange={setDateEvent} />
-          </div>
-        </Modal>
-      </When>
+        <div className="flex w-full h-[60px]">
+          <Button isLoading={isLoading}>{'Enviar'}</Button>
+        </div>
+      </form>
+      <Modal closeModal={handleCloseCalendar} show={showCalendar} closeOnBackDrop>
+        <div className="rounded-xl">
+          <Calendar value={dateEvent} onChange={setDateEvent} />
+        </div>
+      </Modal>
     </div>
   );
 }
