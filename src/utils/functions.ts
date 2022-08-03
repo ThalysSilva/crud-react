1234f7580e3e81cda3ae7a2f9c2dbb7219eeb0ea
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-03 18:54:52 -0300

    feat: add conditional if string is empty

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -9,18 +9,18 @@ export function capitalize(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-export function normalizeCharacter(character: number | string){
-  const normalizedCharacter = 
+export function normalizeCharacter(character: number | string) {
+  if (typeof character === 'string' && character.length) return '00';
+  const normalizedCharacter =
     character.toString().length == 1 ? '0' + character.toString() : character.toString();
   return normalizedCharacter;
 }
 
-export function reverseString(string: string){
-  return string
+export function reverseString(string: string) {
+  return string;
 }
 
 export function normalizeDateToString(date: Date) {
-
   const day = normalizeCharacter(date.getDate());
   const month = normalizeCharacter(date.getUTCMonth() + 1);
   const Year = normalizeCharacter(date.getFullYear());
