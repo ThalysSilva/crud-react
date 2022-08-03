a866bb9cc8a554f989afc875b60508f025f6ecc5
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-03 01:22:56 -0300

    feat: change size of text in TextArea

--- src/components/shared/textArea/index.tsx
+++ src/components/shared/textArea/index.tsx
@@ -8,7 +8,7 @@ export function TextArea({ className, ...rest }: Props) {
   return (
     <textarea
     {...rest}
-      className={`flex flex-row py-2  px-4 gap-4 bg-white/10 text-white w-full h-10 rounded-xl focus:outline-none ${className ? className : ''}`}
+      className={`flex flex-row py-2  px-4 gap-4 bg-white/10 text-white text-sm sm:text-base w-full h-10 rounded-xl focus:outline-none ${className ? className : ''}`}
     />
   );
 }
