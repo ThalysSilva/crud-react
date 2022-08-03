8443defa4c613db3d91d142941ddd2d764c4a726
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-03 18:59:42 -0300

    feat: create handleCreateEventClick function

--- src/hooks/pageEvents/useEvents.ts
+++ src/hooks/pageEvents/useEvents.ts
@@ -1,9 +1,18 @@
+import { useRouter } from 'next/router';
+import config from '../../config';
 import { useAuthContext } from '../useAuthContext';
 
 export default function useEvents() {
+  const router = useRouter();
   const { signOut } = useAuthContext();
+  const { events } = config.webRoutes;
+
+  function handleCreateEventClick() {
+    router.push(events.create);
+  }
 
   return {
+    handleCreateEventClick,
     signOut
   };
 }
