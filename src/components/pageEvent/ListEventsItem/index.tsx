94685a37feb110784e880ee1d517aeb8e24d4b7d
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-03 19:45:37 -0300

    feat: using hook

--- src/components/pageEvent/ListEventsItem/index.tsx
+++ src/components/pageEvent/ListEventsItem/index.tsx
@@ -5,6 +5,7 @@ import TrashIcon from '../../../assets/icon/Trash';
 import config from '../../../config';
 import { H2 } from '../../shared/Texts';
 import { ListEventsItemContent } from '../ListIEventstemContent';
+import useListEventsItems from './hooks/useListEventsItem';
 import { DailyEvent } from './types';
 
 type Props = {
@@ -12,25 +13,16 @@ type Props = {
 };
 
 export function ListEventsItem({ evento }: Props) {
-  const [color, setColor] = useState('#fff');
-  const { events } = config.webRoutes;
+  const { color, handleEditItem } = useListEventsItems(evento);
 
-  const router = useRouter();
-
-  function editItem(id: string) {
-    router.push(events.base + '/' + id);
-  }
-  useEffect(() => {
-    setColor(evento.cor);
-  }, [evento.cor]);
   return (
     <div className="flex flex-row h-[220px] sm:h-[245px] w-full items-center bg-gray-700 p-4 rounded-xl mb-6">
-      <div className={`flex  h-full w-1 rounded-full bg-[${color}] mr-4`} />
+      <div className={`flex  h-full w-1 rounded-full mr-4`} style={{ backgroundColor: color }} />
       <div className={'flex flex-1 flex-col'}>
         <div className={'flex flex-row justify-between items-center mb-4'}>
           <H2>{evento.titulo}</H2>
           <div className={'flex flex-row justify-center items-start gap-6'}>
-            <button onClick={() => editItem(evento.id.toString())}>
+            <button onClick={handleEditItem}>
               <PenIcon />
             </button>
             <button>
