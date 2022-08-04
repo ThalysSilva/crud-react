03165d6a797b67dba4c1c16977131a5bbc3c65f1
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-04 13:13:36 -0300

    feat: change directory name

--- src/components/pageEvent/ListIEventsItemContent/index.tsx
+++ src/components/pageEvent/ListIEventsItemContent/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { H3, H4, H5 } from '../../shared/Texts';
+import { Text3, Text4, Text5 } from '../../shared/Texts';
 import { DailyEvent } from '../ListEventsItem/types';
 
 type Props = {
@@ -11,26 +11,26 @@ export function ListEventsItemContent({ evento }: Props) {
   return (
     <div className={'grid grid-rows-2 gap-2'}>
       <div>
-        <H3>{'Descrição'}</H3>
-        <H4>{descricao}</H4>
+        <Text3>{'Descrição'}</Text3>
+        <Text4>{descricao}</Text4>
       </div>
 
       <div className={'flex flex-row justify-between items-center gap-6'}>
         <div className={'max-w-[100px]'}>
-          <H3>{'Data'}</H3>
-          <H4>{data}</H4>
+          <Text3>{'Data'}</Text3>
+          <Text4>{data}</Text4>
         </div>
 
         <div className={'flex flex-col '}>
         
           <div className={'flex flex-col'}>
-            <H4>{'Criado em'}</H4>
-            <H5>{created_at ? created_at : '- -'}</H5>
+            <Text4>{'Criado em'}</Text4>
+            <Text5>{created_at ? created_at : '- -'}</Text5>
           </div>
 
           <div className={'flex flex-col'}>
-            <H4 >{'Atualizado em'}</H4>
-            <H5>{updated_at ? updated_at : '- -'}</H5>
+            <Text4 >{'Atualizado em'}</Text4>
+            <Text5>{updated_at ? updated_at : '- -'}</Text5>
           </div>
         </div>
       </div>
