66c92a2baa644770be37087344463841af7f43c8
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-04 16:44:27 -0300

    feat: adding boxShadow variables in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       keyframes: {
-
         fadeInAnimation: {
-          "0%": { opacity: 0 },
+          '0%': { opacity: 0 },
 
-          "100%": { opacity: 1 },
-        },
+          '100%': { opacity: 1 }
+        }
       },
-
+      boxShadow: {
+        'inset-y': 'inset 0px 50px 20px -20px, inset 0px -50px 20px -20px ; ',
+        'bottom': '0px 10px 5px ',
+        'top': '0px -10px 5px'
+      },
       animation: {
-        heightModalAnimation: "heightModalAnimation 500ms ease-in-out",
-      },
-    },
+        heightModalAnimation: 'heightModalAnimation 500ms ease-in-out'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
+
+
