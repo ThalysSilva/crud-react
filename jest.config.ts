c70e9819f4fce2b6b15bf9da2493df1b4df30001
Thalys Farias da Silva <thalysfarias11@gmail.com>
2022-08-04 22:23:08 -0300

    fix: resolving wrong jest config

--- jest.config.ts
+++ jest.config.ts
@@ -89,7 +89,7 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "identity-obj-proxy"
+    ".+\\.(css|style|less|sass|scss|png|jpg|ttf|woff|woff2)$": "identity-obj-proxy"
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
